b84ed62306f85d1505c294169b2b9b93766d2768
man36725 <62794328+man36725@users.noreply.github.com>
2021-04-16 08:00:53 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,18 +1,24 @@
-#include <stdio.h>
+#include<stdio.h>
 #include "flames_calculator.h"
-int flame_calculator(char* y, char* p);
+int strlength(char* y, char* p);
 int main()
 {
 	char y[20];
 	char p[20];
-	printf("***********************FLAMES*********************");
+	int u;
+	printf("***********************FLAMES*********************\n");
 	printf("Enter Your name:");
 	scanf("%s",y);
 	printf("Enter Your Partner name:");
 	scanf("%s",p);
 	printf("Fetching results.............\n");
 	printf("You and Your partner are....");
-	flame_calculator(y,p);
-	
-	
+	strlength(y,p);
+	printf("\n\nTo continue Enter 1 else any number:: ");
+	scanf("%d",&u);
+	if(u==1)
+		main();
+	else 
+		printf("\n\nThank You\n\n");
+
 }
